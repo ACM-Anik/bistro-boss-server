1d00663495ff450c6cab87d3293bbcbc1e6c4a7d
ACM-Anik <anikmojumder5252@gmail.com>
2023-06-04 04:59:05 +0600

    payment method added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_kEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -12,14 +13,15 @@ app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if(!authorization){
-        return res.status(401).send({error:true, message: 'Unauthorized access'});
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorized access' });
     }
     // Bearer token
     const token = authorization.split(' ')[1];
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err){
-            return res.status(401).send({error:true, message: 'Unauthorized access'})
+        if (err) {
+            return res.status(401).send({ error: true, message: 'Unauthorized access' })
         }
         req.decoded = decoded;
         next();
@@ -51,31 +53,32 @@ async function run() {
         const menuCollection = client.db("bistroBoss").collection("menu");
         const reviewCollection = client.db("bistroBoss").collection("reviews");
         const cartCollection = client.db("bistroBoss").collection("carts");
+        const paymentCollection = client.db("bistroBoss").collection("payments");
 
-        // JWT
+        // JWT:-
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
 
-            res.send({token});
+            res.send({ token });
         })
 
         // Warning: Use verifyJWT before using verifyAdmin
         const verifyAdmin = async (req, res, next) => {
             const email = req.decoded.email;
-            const query = {email: email}
+            const query = { email: email }
             const user = await userCollection.findOne(query);
-            if(user?.role !== 'admin'){
-                return res.status(403).send({error: true, message: 'Forbidden message!'})
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'Forbidden message!' })
             }
-            next(); 
+            next();
         }
 
         /**
          * 1. Don't show secure link to those who should not see the links
          * 2. Use jwt token: verifyJWT
          * 3. Use verifyAdmin
-         */ 
+         */
 
 
         // Users related APIs:--
@@ -84,13 +87,13 @@ async function run() {
             res.send(result);
         })
 
-        app.post('/users', async ( req, res) => {
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = {email: user.email};
+            const query = { email: user.email };
             const existingUser = await userCollection.findOne(query);
 
-            if(existingUser){
-                return res.send({message: 'user already exists!'})
+            if (existingUser) {
+                return res.send({ message: 'user already exists!' })
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
@@ -100,21 +103,23 @@ async function run() {
         // Email same
         // check admin
         app.get('/users/admin/:email', verifyJWT, async (req, res) => {
-            const email= req.params.email;
+            const email = req.params.email;
 
-            if(req.decoded.email !== email){
-                res.send({admin: false})
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
             }
 
-            const query = {email: email};
+            const query = { email: email };
             const user = await userCollection.findOne(query);
-            const result = {admin: user?.role === 'admin'};
+            const result = { admin: user?.role === 'admin' };
             res.send(result);
         })
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            console.log("Admin:id--patch=>". id);
+
+            const filter = { _id: new ObjectId(id) };
             const updateDoc = {
                 $set: {
                     role: 'admin'
@@ -149,7 +154,7 @@ async function run() {
 
         app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await menuCollection.deleteOne(query);
             res.send(result);
         })
@@ -172,8 +177,8 @@ async function run() {
             }
 
             const decodedEmail = req.decoded.email;
-            if(email !== decodedEmail){
-                return res.status(403).send({message: 'Forbidden access!'})
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'Forbidden access!' })
             }
 
             const query = { email: email };
@@ -193,6 +198,35 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
             res.send(result);
+        })
+
+
+
+        // Create Payment intent
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+            // console.log(price, amount);
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
+
+        // Payment related api
+        app.post('/payments', verifyJWT, async (req, res) => {
+            const payment = req.body;
+            const insertResult = await paymentCollection.insertOne(payment);
+
+            const query = {_id: {$in: payment.cartItems.map(id => new ObjectId(id)) } };
+            const deleteResult = await cartCollection.deleteMany(query);
+
+            res.send({insertResult, deleteResult});
         })
 
 
